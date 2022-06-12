a2b1259fb33dce8098049fcd2d105eb50ce2e127
piotr <piotr.dominik.sobol@gmail.com>
2022-06-12 18:21:42 +0200

    Finding circles encircled by a triangles

---
+++ src/voronoi/Circle.java
@@ -0,0 +1,13 @@
+package voronoi;
+
+public class Circle {
+
+    Point centre;
+    double radius;
+
+    public Circle(Point point, int r) {
+        this.centre = point;
+        this.radius = r;
+    }
+
+}
--- src/voronoi/Frame.java
+++ src/voronoi/Frame.java
@@ -132,6 +132,43 @@ public class Frame extends JFrame {
         }
     }
 
+    Circle findCircle(Point p1, Point p2, Point p3){
+
+        int x12 = p1.x - p2.x;
+        int x13 = p1.x - p3.x;
+        int y12 = p1.y - p2.y;
+        int y13 = p1.y - p3.y;
+        int y31 = p3.y - p1.y;
+        int y21 = p2.y - p1.y;
+        int x31 = p3.x - p1.x;
+        int x21 = p2.x - p1.x;
+        int sx13 = (int)(Math.pow(p1.x, 2) -
+                Math.pow(p3.x, 2));
+        int sy13 = (int)(Math.pow(p1.y, 2) -
+                Math.pow(p3.y, 2));
+        int sx21 = (int)(Math.pow(p2.x, 2) -
+                Math.pow(p1.x, 2));
+        int sy21 = (int)(Math.pow(p2.y, 2) -
+                Math.pow(p1.y, 2));
+        int f = ((sx13) * (x12)
+                + (sy13) * (x12)
+                + (sx21) * (x13)
+                + (sy21) * (x13))
+                / (2 * ((y31) * (x12) - (y21) * (x13)));
+        int g = ((sx13) * (y12)
+                + (sy13) * (y12)
+                + (sx21) * (y13)
+                + (sy21) * (y13))
+                / (2 * ((x31) * (y12) - (x21) * (y13)));
+        int c = -(int)Math.pow(p1.x, 2) - (int)Math.pow(p1.y, 2) -
+                2 * g * p1.x - 2 * f * p1.y;
+        int h = -g;
+        int k = -f;
+        double sqr_of_r = h * h + k * k - c;
+        double r = Math.sqrt(sqr_of_r);
+        return new Circle(new Point(h, k), (int)r);
+    }
+
     public void paint(Graphics g)
     {
         Graphics2D g2D = (Graphics2D) g;
@@ -171,5 +208,24 @@ public class Frame extends JFrame {
             number_of_color_b-=20;
             counter++;
         }
+
+        counter=0;
+        for(int i=0; i<5; i++) {
+            for(int j=0; j<5; j++) {
+                for(int k=0; k<5; k++) {
+                    if(i!=j&&j!=k&&i!=k) {
+                        Circle circle = findCircle(points[i], points[j], points[k]);
+                        g2D.setStroke(new BasicStroke(1));
+                        g2D.setPaint(Color.magenta);
+                        g2D.drawOval(circle.centre.x, circle.centre.y, 5, 5);
+                    }
+                }
+            }
+        }
+
+        /*Circle circle1 = new Circle(points[0], (int)Math.sqrt(Math.pow((points[0].x+points[1].x),2)+Math.pow((points[0].y+points[1].y),2)));
+        g2D.setStroke(new BasicStroke(1));
+        g2D.setPaint(Color.BLACK);
+        g2D.drawOval(circle1.centre.x, circle1.centre.y, (int)circle1.radius, (int)circle1.radius);*/
     }
 }
